c8e8f7aa8a4ad9efab42beac326170b6be2a5edf
Bence Kovacs <kov.benc@gmail.com>
2023-03-05 11:16:34 +0100

    Update Echo.swift

--- src/ios/Echo.swift
+++ src/ios/Echo.swift
@@ -15,4 +15,117 @@ public class Echo : CDVPlugin {
 
     self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
   }
+  
+  @objc
+  func shortvibra(_ command: CDVInvokedUrlCommand) {
+    let pluginResult:CDVPluginResult
+    
+    let siker = false
+
+    let parameters = [
+      CHHapticEventParameter(
+        parameterID: .hapticIntensity,
+        value: 1.00),
+      CHHapticEventParameter(
+        parameterID: .hapticSharpness,
+        value: 1.00),
+      CHHapticEventParameter(
+        parameterID: .attackTime,
+        value: -1.00),
+      CHHapticEventParameter(
+        parameterID: .decayTime,
+        value: 1.00),
+      CHHapticEventParameter(
+        parameterID: .releaseTime,
+        value: 0.00),
+      CHHapticEventParameter(
+        parameterID: .sustained,
+        value: 1)
+    ]
+
+    let event = CHHapticEvent(
+      eventType: .hapticContinuous,
+      parameters: parameters,
+      relativeTime: 0,
+      duration: 0.17)
+
+    let engine = try CHHapticEngine()
+    try engine.start()
+
+    let pattern = try CHHapticPattern(
+      events: [event],
+      parameters: [])
+    let player = try engine
+      .makePlayer(with: pattern)
+    try {
+      player.start(atTime: 0)
+      siker = true
+    }
+    
+    if (siker==true) {
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK)
+    } else {
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
+    }
+
+    self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+  }
+  
+   @objc
+  func longvibra(_ command: CDVInvokedUrlCommand) {
+    let pluginResult:CDVPluginResult
+    
+    let siker = false
+
+    let parameters = [
+      CHHapticEventParameter(
+        parameterID: .hapticIntensity,
+        value: 1.00),
+      CHHapticEventParameter(
+        parameterID: .hapticSharpness,
+        value: 1.00),
+      CHHapticEventParameter(
+        parameterID: .attackTime,
+        value: -1.00),
+      CHHapticEventParameter(
+        parameterID: .decayTime,
+        value: 1.00),
+      CHHapticEventParameter(
+        parameterID: .releaseTime,
+        value: 0.00),
+      CHHapticEventParameter(
+        parameterID: .sustained,
+        value: 1)
+    ]
+
+    let event = CHHapticEvent(
+      eventType: .hapticContinuous,
+      parameters: parameters,
+      relativeTime: 0,
+      duration: 0.57)
+
+    let engine = try CHHapticEngine()
+    try engine.start()
+
+    let pattern = try CHHapticPattern(
+      events: [event],
+      parameters: [])
+    let player = try engine
+      .makePlayer(with: pattern)
+    try {
+      player.start(atTime: 0)
+      siker = true
+    }
+    
+    if (siker==true) {
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK)
+    } else {
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_ERROR)
+    }
+
+    self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+  }
+  
+
+  
 }
