14bfe68ccb08c6320878de21e6fc618772248d45
Bence Kovacs <kov.benc@gmail.com>
2023-03-05 11:10:48 +0100

    Update echo.js
    added shortvibra and longvibra feature

--- www/echo.js
+++ www/echo.js
@@ -3,3 +3,18 @@ window.echo = function(str, callback) {
       callback('Nothing to echo.');
   }, "Echo", "echo", [str]);
 };
+
+window.shortvibra = function(callback) {
+  cordova.exec(callback, function(err) {
+      callback('shortvibra error');
+  }, "Echo", "shortvibra");
+};
+
+window.longvibra = function(callback) {
+  cordova.exec(callback, function(err) {
+      callback('longvibra error');
+  }, "Echo", "longvibra");
+};
+
+
+
